5a1ea89a2fa8d38c55e0ba3618a5124a74d9921e
Murt05 <murtadhajaafar47@gmail.com>
2024-04-25 20:08:37 +0100

    Initial commit

---
+++ Horse.java
@@ -0,0 +1,86 @@
+
+/**
+ * Write a description of class Horse here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Horse
+{
+    //Fields of class Horse
+    
+    String horseName;
+    char horseSymbol;
+    int distanceTravelled;
+    boolean hasFallen;
+    double horseConfidence;
+      
+    //Constructor of class Horse
+    /**
+     * Constructor for objects of class Horse
+     */
+    public Horse(char horseSymbol, String horseName, double horseConfidence)
+    {
+        this.horseSymbol = horseSymbol;
+        this.horseName = horseName;
+        this.horseConfidence = horseConfidence;
+        this.distanceTravelled = 0;
+        this.hasFallen = false;
+
+    }
+    
+    
+    
+    //Other methods of class Horse
+    public void fall()
+    {
+        hasFallen = true;
+    }
+    
+    public double getConfidence()
+    {
+        return horseConfidence;
+    }
+    
+    public int getDistanceTravelled()
+    {
+        return distanceTravelled;
+    }
+    
+    public String getName()
+    {
+        return horseName;
+    }
+    
+    public char getSymbol()
+    {
+        return horseSymbol;
+    }
+    
+    public void goBackToStart()
+    {
+        distanceTravelled = 0;
+    }
+    
+    public boolean hasFallen()
+    {
+        if (hasFallen) {return true;}
+        else {return false;}
+    }
+
+    public void moveForward()
+    {
+        distanceTravelled++;
+    }
+
+    public void setConfidence(double newConfidence)
+    {
+        horseConfidence = newConfidence;
+    }
+    
+    public void setSymbol(char newSymbol)
+    {
+        horseSymbol = newSymbol;
+    }
+    
+}
